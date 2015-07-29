023b3636a0dbaf2bba165c3e2bf544e2c6e71aff
Martin Cross <mncross@gmail.com>
2015-07-29 18:03:59 -0500

    implemented arrays to display grade counts to DOM

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-// Create an array of student scores and standard letter grades
+// Create an array of student scores
 var scores = [75, 52, 76, 89, 98, 100, 63, 72, 59, 95];
-// var letter_grades = [A, B, C, D, F]
 
-// Create a way to track the count of each grade
-var countA = 0;
-var countB = 0;
-var countC = 0;
-var countD = 0;
-var countF = 0;
+// Create and array of standard letter grades
+var letter_grades_counts = [0, 0, 0, 0, 0];
+var letter_grades = ["A", "B", "C", "D", "F"]
 
 // Create global variables for grade comparisons
 var lowest_score = scores[0];
@@ -17,15 +13,15 @@ var highest_score = scores[0];
 for (var i = 0; i < scores.length; i++) {
   // Convert scores into letter grades
   if (scores[i] >= 91 && scores[i] <= 100) {
-    countA++;
+    letter_grades_counts[0]++;
   } else if (scores[i] >= 81 && scores[i] <= 90) {
-    countB++;
+    letter_grades_counts[1]++;
   } else if (scores[i] >= 71 && scores[i] <= 80) {
-    countC++;
+    letter_grades_counts[2]++;
   } else if (scores[i] >= 61 && scores[i] <= 70) {
-    countD++;
+    letter_grades_counts[3]++;
   } else {
-    countF++;
+    letter_grades_counts[4]++;
   };
   // Determine lowest score
   if (scores[i] < lowest_score) {
@@ -37,14 +33,6 @@ for (var i = 0; i < scores.length; i++) {
   }
 }
 
-// Console log all results
-console.log("There are", countA, "A's");
-console.log("There are", countB, "B's");
-console.log("There are", countC, "C's");
-console.log("There are", countD, "D's");
-console.log("There are", countF, "F's");
-console.log("The lowest score was", lowest_score);
-console.log("The highest score was", highest_score);
 
 // Create global variables for references to DOM elements
 var grade_totals = document.getElementById("grade-totals");
@@ -52,10 +40,25 @@ var lowest_grade = document.getElementById("lowest-grade");
 var highest_grade = document.getElementById("highest-grade");
 
 // Return count of letter grades to DOM
+var output = ""
+for (var i = 0; i < letter_grades_counts.length; i++) {
+  output += "<div>There are " + letter_grades_counts[i] + " " + letter_grades[i];
+  if (letter_grades_counts[i] > 1) {
+    output += "'s</div>";
+  } else {
+    output += "</div>";
+  }
+}
 
-// for (var i = 0; i < letter_grades.length; i++) {
-//   grade_totals.innerHTML = "<div>There are " +  +  + "'s</div>"
-// }
+grade_totals.innerHTML = output;
+lowest_grade.innerHTML = "<div>The lowest score is " + lowest_score + "</div>";
+highest_grade.innerHTML = "<div>The highest score is " + highest_score + "</div>";
 
-lowest_grade.innerHTML = "<div>" + lowest_score + "</div>";
-highest_grade.innerHTML = "<div>" + highest_score + "</div>";
+// Console log all results
+console.log("There are", letter_grades_counts[0], "A's");
+console.log("There are", letter_grades_counts[1], "B's");
+console.log("There are", letter_grades_counts[2], "C's");
+console.log("There are", letter_grades_counts[3], "D's");
+console.log("There are", letter_grades_counts[4], "F's");
+console.log("The lowest score was", lowest_score);
+console.log("The highest score was", highest_score);
